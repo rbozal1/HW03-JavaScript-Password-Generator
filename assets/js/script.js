428dff2e5d279d0616cc630ff434d8055a70fa21
Roger Steve <roxhersbozali@gmail.com>
2020-09-07 20:46:57 -0400

    minor changes for readme,style.css and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-//possible password values
+// set possible password values
+
 // array of special characters 
 var specialChar = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'
 ];
@@ -13,17 +14,13 @@ var lowerCasedChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
 //array of numeric characters
 var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// variable declaration
-
-var passwordLength = "";
-var confirmSpecialChar = "";
-var confirmLowerChar = "";
-var confirmUpperChar = "";
-var confirmNumeriChar = "";
-
+//set variables for later use
+var values = "";
+var randomPassword = "";
+// generate a password with randomly selected characters with maximum numbers
 function generatePassword() {
 var passwordLength = (prompt("How many characters would you like to use for your password?"));
-  //check if it meets the condition
+  //loop through the requirements of the password length
   while(passwordLength <= 7 || passwordLength >= 129) {
     alert("Password is invalid! Must be between 8 to 128 characters.Please try again");
     var passwordLength = (prompt("How many characters would you like to use for your password?"));
@@ -31,10 +28,13 @@ var passwordLength = (prompt("How many characters would you like to use for your
     
     alert("Your password will consist" + " " + passwordLength + " " + "characters");
   
-
+// confirm whether the user wants to use special characters
 var confirmSpecialChar = confirm("Would you like to use special characters?");
-var confirmNumeriChar = confirm("Would you like to use lower case characters ?");    
+// confirm whether the user wants to use lowercase characters
+var confirmNumeriChar = confirm("Would you like to use lower case characters ?");
+// confirm whether the user wants to use uppercase characters    
 var confirmLowerChar = confirm("Would you like to use upper case characters ?");
+// confirm whether the user wants to use numeric characters
 var confirmUpperChar = confirm("Would you like to use numeric characters ?"); 
 
 while(confirmUpperChar === false && confirmLowerChar === false && confirmSpecialChar === false && confirmNumeriChar === false) {
@@ -44,14 +44,6 @@ var confirmNumeriChar = confirm("Would you like to use lower case characters?");
 var confirmLowerChar = confirm("Would you like to use upper case characters?");
 var confirmUpperChar = confirm("Would you like to use numeric characters?"); 
 } 
-
-
-var values = [
-  // specialChar,
-  // upperCasedChar,
-  // lowerCasedChar,
-  // numericChar
-]
 
 if (confirmSpecialChar) {
   values = values.concat(specialChar)
@@ -68,15 +60,16 @@ if (confirmLowerChar) {
 if (confirmNumeriChar) {
   values = values.concat(numericChar)
 }
-console.log(values)
 
-var RandomPassword = ""
+// console.log(values)
 
 for(var i = 0; i < passwordLength; i++) {
-  RandomPassword = RandomPassword + values[Math.floor(Math.random() * values.length)];
+  randomPassword += values.charAt(Math.floor(Math.random() * values.length)
+  );
 }
-return RandomPassword;
+return randomPassword;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
